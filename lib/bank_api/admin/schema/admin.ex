780ede4f30b21860472f05f06d13bf0899d45bac
Tarcisio Oliveira <tarcisio@ymail.com>
2021-08-31 11:05:48 -0300

    refactoring cast user and admin

--- lib/bank_api/admin/schema/admin.ex
+++ lib/bank_api/admin/schema/admin.ex
@@ -18,25 +18,26 @@ defmodule BankApi.Admins.Schemas.Admin do
     timestamps()
   end
 
+  @request_params [:email, :password, :password_confirmation]
   @doc false
   def changeset(params) do
     %__MODULE__{}
-    |> cast(params, [:email, :password, :password_confirmation])
-    |> validate_required([:email, :password, :password_confirmation])
-    |> validate_format(:email, ~r/@/, message: "Invalid format email.")
+    |> cast(params, @request_params)
+    |> validate_required(@request_params)
+    |> validate_format(:email, ~r/@/, message: "Email format invalid.")
+    |> unique_constraint(:email, message: "Email already in use.")
+    |> validate_confirmation(:password, message: "Differents password.")
     |> validate_length(:password,
       min: 6,
       max: 10,
-      message: "Password must accountin between 4 and 10 characters."
+      message: "Password must contain between 4 and 10 characters."
     )
-    |> validate_confirmation(:password, message: "Differents password.")
-    |> unique_constraint(:email, message: "Email already in use.")
-    |> put_password()
+    |> put_pass_hash()
   end
 
-  defp put_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
     put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(password))
   end
 
-  defp put_password(changeset), do: changeset
+  defp put_pass_hash(changeset), do: changeset
 end
--- lib/bank_api/user/schema/user.ex
+++ lib/bank_api/user/schema/user.ex
@@ -22,20 +22,18 @@ defmodule BankApi.Users.Schemas.User do
   @request_params [:email, :name, :password, :password_confirmation]
 
   @doc false
-  def build(params) do
-    params
-    |> changeset()
-    |> apply_action!(:insert)
-  end
-
-  @doc false
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @request_params)
     |> validate_required(@request_params)
-    |> validate_format(:email, ~r/@/, message: "Email format invalid")
-    |> unique_constraint(:email, message: "Email already used")
-    |> validate_confirmation(:password, message: "Passwords are different")
+    |> validate_format(:email, ~r/@/, message: "Email format invalid.")
+    |> unique_constraint(:email, message: "Email already used.")
+    |> validate_confirmation(:password, message: "Passwords are different.")
+    |> validate_length(:password,
+      min: 6,
+      max: 10,
+      message: "Password must contain between 4 and 10 characters."
+    )
     |> put_pass_hash
   end
 
