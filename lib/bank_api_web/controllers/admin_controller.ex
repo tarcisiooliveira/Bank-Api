32a51240b120836ccbf327ea20bbb161a5f45314
Tarcisio Oliveira <tarcisio@ymail.com>
2021-09-08 22:22:52 -0300

    changing order between functions

--- lib/bank_api_web/controllers/admin_controller.ex
+++ lib/bank_api_web/controllers/admin_controller.ex
@@ -8,6 +8,36 @@ defmodule BankApiWeb.AdminController do
   action_fallback BankApiWeb.FallbackController
 
   @doc """
+  Create an Admin
+
+  ## Parameters
+
+    * `email` - String email of the admin
+    * `password` - String password of the admin
+    * `password_confirmation` - String password confirmation of the admin
+
+  ## Examples
+      iex> sign_up(%{"email" => "admin@admin.com", "password" => "123456", "password_confirmation" => "123456"})
+      %{
+        "admin" => %{
+          "email" => "admin@admin.com",
+          "id" => _new_id
+        }
+      }
+
+      iex> sign_up(%{"email" => "test2@admin.com", "password" => "123456"})
+      %{"email" => "test2@admin.com", "password" => "123456"}
+  """
+  def sign_up(conn, params) do
+    with {:ok, %{insert_admin: %Admin{id: id, email: email}}} <- SignUp.create(params) do
+      conn
+      |> put_status(:ok)
+      |> put_view(BankApiWeb.AdminView)
+      |> render("sign_up.json", %{id: id, email: email})
+    end
+  end
+
+  @doc """
   Valid an Admin, generate a token
 
   ## Parameters
@@ -29,37 +59,6 @@ defmodule BankApiWeb.AdminController do
       |> put_status(:ok)
       |> put_view(BankApiWeb.AdminView)
       |> render("sign_in.json", token: token)
-    end
-  end
-
-  @doc """
-  Create an Admin
-
-  ## Parameters
-
-    * `email` - String email of the admin
-    * `password` - String password of the admin
-    * `password_confirmation` - String password confirmation of the admin
-
-  ## Examples
-      iex> sign_up(%{"email" => "test22@admin.com", "password" => "123456", "password_confirmation" => "123456"})
-      %{
-        "admin" => %{
-          "email" => "test22@admin.com",
-          "id" => _new_id
-          },
-          "message" => "Admin Created."
-        }
-
-      iex> sign_up(%{"email" => "test2@admin.com", "password" => "123456"})
-      %{"email" => "test2@admin.com", "password" => "123456"}
-  """
-  def sign_up(conn, params) do
-    with {:ok, %{insert_admin: %Admin{id: id, email: email}}} <- SignUp.create(params) do
-      conn
-      |> put_status(:ok)
-      |> put_view(BankApiWeb.AdminView)
-      |> render("sign_up.json", %{id: id, email: email})
     end
   end
 end
